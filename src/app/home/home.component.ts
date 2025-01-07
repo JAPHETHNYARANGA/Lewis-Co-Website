372970f2e556b3268d434e76e13b3f6bf1b03ea4
JAPHETHNYARANGA <nyaranga4@gmail.com>
2025-01-07 12:23:56 +0300

    added some navigation links to homepage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from "../shared/navbar/navbar.component";
 import { MainCardsComponent } from "../shared/main-cards/main-cards.component";
 import { FooterComponent } from "../shared/footer/footer.component";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -11,5 +12,20 @@ import { FooterComponent } from "../shared/footer/footer.component";
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  constructor(private router: Router) {}
+
+
+  navigateToContact(){
+    this.router.navigate(['contact']);
+  }
+  navigateToTech(){
+    this.router.navigate(['tech'])
+  }
+  navigateToMarketing(){
+    this.router.navigate(['marketing'])
+  }
+  navigateToAutomation(){
+    this.router.navigate(['automation'])
+  }
 
 }
